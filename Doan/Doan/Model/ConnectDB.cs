345daf5b3e57d432a61e7d35d2d5ebe001da5218
Lamduy0807 <19521421@gm.uit.edu.vn>
2021-11-14 10:43:55 +0700

    connect database

---
+++ Doan/Doan/Model/ConnectDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doan.Model
+{
+    class ConnectDB
+    {
+        SqlConnection connect;
+        public ConnectDB()
+        {
+            this.connect = new SqlConnection(@"Data Source = .\SQLEXPRESS; Initial Catalog = doan; Integrated Security = True");
+        }
+    }
+
+}
