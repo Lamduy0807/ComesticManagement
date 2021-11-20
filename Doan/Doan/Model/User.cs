4063cb7d769c12df297e519f4bf7e69ebfe07b7a
thanhnga0104 <19521880@gm.uit.edu.vn>
2021-11-20 19:52:07 +0700

    feature of Employee

--- Doan/Doan/Model/User.cs
+++ Doan/Doan/Model/User.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Doan.Model
 {
@@ -10,6 +12,13 @@ namespace Doan.Model
     {
         public string Username { get; set; }
         public string Password { get; set; }
+        public string Employee_id { get; set; }
+        public string EmployName { get; set; }
+        public string Citizen_id { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Position { get; set; }
         public bool CheckValidate(string username, string password)
         {
             ConnectDB connect = new ConnectDB();
@@ -21,6 +30,39 @@ namespace Doan.Model
             else
                 return false;
 
+        }
+
+        public DataTable LoadListEmployee()
+        {
+            ConnectDB connect = new ConnectDB();
+            string sqlQuery = "Select * from Employee";
+            /*DataSet list = new DataSet();*/
+            return connect.GetData(sqlQuery);
+
+        }
+
+        public bool AddEmployee(string name, string citizen_id, string email, string phone, string position, string address)
+        {           
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO Employee (EmployName, Citizen_id, Address, PhoneNumber, Email, Position, Username, Password) VALUES (@name, @citizen_id, @address, @phone, @email, @position, @username, @password)");
+            
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@citizen_id", citizen_id);
+            cmd.Parameters.AddWithValue("@email", email);
+            cmd.Parameters.AddWithValue("@phone", phone);
+            cmd.Parameters.AddWithValue("@position", position);
+            cmd.Parameters.AddWithValue("@address", address);
+            
+            cmd.Parameters.AddWithValue("@username", email);
+            cmd.Parameters.AddWithValue("@password", citizen_id);
+           
+            ConnectDB connect = new ConnectDB();
+            if (connect.AddData(cmd))
+            {
+                return true;
+            }
+            else
+                return false;
         }
     }
 }
--- Doan/Doan/View/Employee/EmployeeForm.cs
+++ Doan/Doan/View/Employee/EmployeeForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Doan.Presenter;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,140 @@ using System.Windows.Forms;
 
 namespace Doan.View.Employee
 {
-    public partial class EmployeeForm : Form
+    public partial class EmployeeForm : Form, IManageEmployee
     {
         public EmployeeForm()
         {
             InitializeComponent();
+        }
+
+        public string Employee_idtext
+        {
+            get
+            {
+                return tbEmployeeID.Text;
+            }
+            set
+            {
+                tbEmployeeID.Text = value;
+            }
+        }
+        public string Nametext
+        {
+            get
+            {
+                return tbName.Text;
+            }
+            set
+            {
+                tbName.Text = value;
+            }
+        }
+        public string Emailtext
+        {
+            get
+            {
+                return tbCitizenID.Text;
+            }
+            set
+            {
+                tbCitizenID.Text = value;
+            }
+        }
+        public string Citizen_idtext
+        {
+            get
+            {
+                return tbEmail.Text;
+            }
+            set
+            {
+                tbEmail.Text = value;
+            }
+        }
+        public string Addresstext
+        {
+            get
+            {
+                return tbAddress.Text;
+            }
+            set
+            {
+                tbAddress.Text = value;
+            }
+        }
+        public string Positiontext
+        {
+            get
+            {
+                return tbPhone.Text;
+            }
+            set
+            {
+                tbPhone.Text = value;
+            }
+        }
+        public string PhoneNumtext
+        {
+            get
+            {
+                return tbPosition.Text;
+            }
+            set
+            {
+                tbPosition.Text = value;
+            }
+        }
+
+        public DataGridView gvEmployee
+        {
+            get { return dgvEmployee; }
+            set { dgvEmployee = value; }
+        }
+        public string Username
+        {
+            get
+            {
+                return tbUsername.Text;
+            }
+            set
+            {
+                tbUsername.Text = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return tbPassword.Text;
+            }
+            set
+            {
+                tbPassword.Text = value;
+            }
+        }
+
+        //public string Username { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        //public string Password { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        /*private void button4_Click(object sender, EventArgs e)
+        {
+
+        }*/
+
+        private void EmployeeForm_Load(object sender, EventArgs e)
+        {
+
+            EmployeePresenter employeePresenter = new EmployeePresenter(this);
+            employeePresenter.LoadListEmployee();
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            EmployeePresenter employeePresenter = new EmployeePresenter(this);
+            employeePresenter.AddData();
+
         }
     }
 }
