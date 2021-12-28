2ed2ad4d1e10f46face2fd71539ad4ad9ed00d4f
Lamduy0807 <19521421@gm.uit.edu.vn>
2021-12-28 19:58:48 +0700

    modify statistic

--- Doan/Doan/View/Statistic/StatisticsForm.cs
+++ Doan/Doan/View/Statistic/StatisticsForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using Doan.Presenter;
 
 namespace Doan.View.Statistic
@@ -18,27 +19,27 @@ namespace Doan.View.Statistic
             InitializeComponent();
         }
 
-        public string Date 
-        { 
-            get { return dateTimePicker1.Text.ToString(); } 
-            set { dateTimePicker1.Text = value; } 
+        public string Date
+        {
+            get { return dateTimePicker1.Text.ToString(); }
+            set { dateTimePicker1.Text = value; }
         }
-        public string RevenueMonth 
-        { 
-            get { return lbRevenueThisMonth.Text; } 
-            set { lbRevenueThisMonth.Text = value; } 
+        public string RevenueMonth
+        {
+            get { return lbRevenueThisMonth.Text; }
+            set { lbRevenueThisMonth.Text = value; }
         }
-        public string RevenueToday 
+        public string RevenueToday
         {
             get { return lbRevenueToday.Text; }
             set { lbRevenueToday.Text = value; }
         }
-        public string SumProduct 
+        public string SumProduct
         {
             get { return lbSumProduct.Text; }
             set { lbSumProduct.Text = value; }
         }
-        public string BillMonth 
+        public string BillMonth
         {
             get { return lbBillThisMonth.Text; }
             set { lbBillThisMonth.Text = value; }
@@ -54,14 +55,43 @@ namespace Doan.View.Statistic
             set { dtgvBestSeller = value; }
         }
 
+        
+        private DataTable _data;
+        public DataTable data { set { _data = value; } }
+
         private void Statistics_Load(object sender, EventArgs e)
         {
             StatisticPresenter statisticPresenter = new StatisticPresenter(this);
-            statisticPresenter.GetBillMonth();
-            statisticPresenter.GetBillToday();
-            statisticPresenter.GetRevenueMonth();
-            statisticPresenter.GetRevenueToday();
+            string sDay = DateTime.Now.ToString("dd");
+            string sMonth = DateTime.Now.ToString("MM");
+            string sYear = DateTime.Now.ToString("yyyy");
+            statisticPresenter.GetBillMonth(sMonth,sYear);
+            statisticPresenter.GetBillToday(sDay,sMonth,sYear);
+            statisticPresenter.GetRevenueMonth(sMonth,sYear);
+            statisticPresenter.GetRevenueToday(sDay, sMonth, sYear);
             statisticPresenter.GetTopProduct();
+            statisticPresenter.GetLineChart(sMonth,sYear);
+            ChartMoneydaybydate();
+        }
+        private void ChartMoneydaybydate()
+        {
+            //chart1.Series["Revenue"].ChartType = SeriesChartType.Line;
+            chart1.Series["Revenue"].XValueType = ChartValueType.DateTime;
+
+            chart1.ChartAreas[0].AxisX.LabelStyle.Format = "dd";
+;
+            chart1.DataSource = _data; 
+            chart1.Series["Revenue"].XValueMember = "Ngay";
+            chart1.Series["Revenue"].YValueMembers = "Tong";
+            chart1.Series["Revenue"].IsValueShownAsLabel = true;
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            StatisticPresenter statisticPresenter = new StatisticPresenter(this);
+            statisticPresenter.RetriveData();
+            ChartMoneydaybydate();
         }
     }
 }
